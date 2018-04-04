d5e5a455f272a3d64075cdba8787decfcd2711a2
pepepaez <jopaez@gmail.com>
2018-04-04 10:42:45 -0700

    Update and rename web.rb to reaction_bot.rb

---
+++ reaction_bot.rb
@@ -0,0 +1,59 @@
+# -*- coding: UTF-8 -*-
+
+# just run this ruby script; no special parameters. a config.yml file in the same
+# directory should look like this:
+#
+# discord:
+#   token: 'a discord api token goes here'
+#   client_id: 'a discord client id goes here'
+#
+# Other than that, set up the bot so it has role permissions to assign and
+# remove roles. This bot is written to watch reactions to posts made to a
+# channel named '#assignment'. If a reaction is added or removed from a post
+# the bot will scan the content of the post for any line that contains that
+# same reaction and a role name mention. If it finds one, it will add or
+# remove the role, depending on whether the reaction was added or removed.
+
+require 'discordrb'
+require 'yaml'
+
+cnf = YAML::load_file(File.join(__dir__, 'config.yml'))
+
+bot = Discordrb::Bot.new token: cnf['discord']['token'], client_id: cnf['discord']['client_id']
+
+def assign_role_by_reaction(event, &block)
+  if event.channel.name == 'role-assignment'
+    # check for role_mentions... if we have some, then this is actionable
+    if not event.message.role_mentions.empty?
+      message_lines = event.message.content.lines
+      message_lines.keep_if { | line | line.include?(event.emoji.name) and line =~ /<@&[0-9]+>/ }
+      if not message_lines.empty?
+        m = message_lines[0].match(/<@&([0-9]+)>/)
+        if m
+          role = event.message.role_mentions.select { | r | r.id.to_s == m[1] }
+          if not role.empty?
+            block.call event.channel.server.member(event.user.id), role[0]
+          end
+        end
+      end
+    end
+  end
+end
+
+bot.reaction_add do |event|
+  assign_role_by_reaction(event) do | member, role |
+    if not member.role?(role)
+      member.add_role(role)
+    end
+  end
+end
+
+bot.reaction_remove do |event|
+  assign_role_by_reaction(event) do | member, role |
+    if member.role?(role)
+      member.remove_role(role)
+    end
+  end
+end
+
+bot.run
